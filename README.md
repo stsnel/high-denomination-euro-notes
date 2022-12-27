6394bb9330a51ab54bcf3ef4bd3f69fa6bf7952f
Sietse Snel <sietsesnel@protonmail.com>
2022-12-27 19:19:31 +0100

    Add relative summary plots, tables, script

--- README.md
+++ README.md
@@ -27,6 +27,69 @@ euro banknotes are reportedly used most frequently, both relative to low-denomin
 in absolute numbers. The repository also contains the processed data and scripts that were used
 to create the graphs and tables.
 
+## Summary data
+
+The tables and plots show the Eurobilltracker denomination data by country over the past (roughly) 20 years.
+Only countries with more than 6000 notes entered (on average, roughly 300 per year) have been included.
+
+|Country|5 euro|10 euro|20 euro|50 euro|100 euro|200 euro|500 euro|
+|-------|------|-------|-------|-------|--------|--------|--------|
+|Andorra|37.4|21.6|19.1|14.8|6.0|0.5|0.7|
+|Austria|44.1|31.9|11.2|7.0|4.7|0.6|0.5|
+|Belgium|45.3|18.7|20.9|13.4|1.2|0.2|0.3|
+|Cyprus|42.5|17.9|26.1|11.7|1.5|0.1|0.2|
+|Estonia|57.3|19.2|15.2|8.0|0.2|0.1|0.1|
+|Finland|33.6|19.6|31.9|13.5|1.0|0.2|0.3|
+|France|33.7|29.4|26.5|9.4|0.9|0.1|0.1|
+|Germany|52.6|22.0|12.5|10.6|1.8|0.2|0.3|
+|Greece|26.9|20.0|24.3|26.1|2.2|0.1|0.2|
+|Ireland|37.3|26.5|23.6|12.2|0.3|0.0|0.0|
+|Italy|27.1|22.8|24.5|23.2|2.1|0.1|0.2|
+|Latvia|28.8|25.3|24.5|20.4|0.8|0.1|0.2|
+|Lithuania|51.8|18.8|11.9|13.0|3.9|0.5|0.1|
+|Luxembourg|33.0|25.3|23.3|15.3|2.2|0.6|0.6|
+|Malta|14.8|40.9|30.5|12.5|0.8|0.2|0.3|
+|Monaco|51.9|13.6|15.9|12.8|4.6|0.7|0.5|
+|Netherlands|36.9|29.2|18.7|14.0|0.9|0.2|0.1|
+|Portugal|23.9|30.7|37.4|6.5|1.0|0.2|0.2|
+|San Marino|40.9|22.9|19.6|14.3|2.1|0.2|0.0|
+|Slovakia|27.3|32.7|18.4|14.1|6.4|0.4|0.7|
+|Slovenia|31.7|25.4|28.9|9.9|3.3|0.3|0.6|
+|Spain|25.5|23.3|26.1|23.2|1.3|0.2|0.4|
+|Vatican City|21.2|17.0|16.3|37.4|7.4|0.4|0.2|
+
+![image](plots/eurozone-summary.png)
+
+|Country|5 euro|10 euro|20 euro|50 euro|100 euro|200 euro|500 euro|
+|-------|------|-------|-------|-------|--------|--------|--------|
+|Belarus|25.7|23.5|33.6|12.4|4.0|0.5|0.4|
+|Bosnia-Herzegovina|35.5|22.5|17.8|17.1|6.2|0.6|0.4|
+|Brazil|10.4|14.2|17.5|31.5|17.2|1.5|7.6|
+|Bulgaria|12.7|16.4|18.5|37.7|11.8|1.7|1.1|
+|Canada|23.7|18.8|34.1|18.2|3.9|0.5|0.8|
+|China|13.6|15.2|18.4|25.0|20.5|1.2|6.0|
+|Croatia|17.3|21.7|20.5|22.9|13.2|1.5|3.0|
+|Czech Republic|27.6|26.8|20.3|16.1|7.2|1.1|0.9|
+|Denmark|23.8|22.2|30.8|18.1|4.0|0.7|0.4|
+|Dominican Republic|6.2|7.5|17.0|43.4|15.1|2.1|8.7|
+|Hungary|22.3|27.3|20.1|20.9|8.0|0.6|0.8|
+|Japan|25.3|30.1|25.3|15.7|3.2|0.2|0.1|
+|Montenegro|22.8|33.6|16.6|23.8|2.8|0.1|0.2|
+|Norway|25.0|19.0|26.0|23.0|6.0|0.3|0.5|
+|Poland|28.3|21.1|17.6|23.7|8.2|0.5|0.6|
+|Romania|25.4|19.8|20.6|23.8|8.2|0.6|1.5|
+|Russia|11.9|12.0|13.7|21.0|33.2|1.6|6.6|
+|Serbia|23.1|25.9|22.4|18.7|8.1|0.9|1.0|
+|Sweden|29.2|24.2|30.7|13.1|2.3|0.3|0.2|
+|Switzerland|42.1|15.3|15.0|18.6|6.5|1.5|1.1|
+|Turkey|24.2|26.4|21.8|18.6|6.3|1.5|1.3|
+|Ukraine|8.1|9.3|9.9|39.3|29.8|1.5|2.0|
+|United Kingdom|9.8|30.0|44.5|13.9|1.5|0.2|0.1|
+|United States|28.6|22.7|27.3|17.2|3.3|0.3|0.5|
+
+![image](plots/non-eurozone-summary.png)
+
+
 ## Sources
 
 * [1] *Banknotes and coins circulation* (2022). European Central Bank (ECB).
---
+++ scripts/plots/gen-relative-circulation-plots.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+
+"""This script creates tables and graphs regarding relative use of high-denomination
+   notes by country"""
+
+import csv
+from glob import glob
+import math
+import os
+
+from matplotlib import pyplot as plt
+
+def get_eurozone_countries():
+    countries = [ "Andorra", "Austria", "Belgium", "Cyprus", "Estonia", "Finland", "France",
+                  "Germany", "Greece", "Ireland", "Italy", "Latvia", "Lithuania",
+                  "Luxembourg", "Malta", "Monaco", "Netherlands", "Portugal", "San Marino",
+                  "Slovakia", "Slovenia", "Spain", "Vatican City" ]
+    return set(countries)
+
+
+def get_summary_data(eurozone=True, minimum_observations = 6000):
+    total_files = glob("../../data/ebt-summary/*.*.total.csv")
+    country_data = dict()
+    eurozone_countries = get_eurozone_countries()
+    for total_file in total_files:
+        filename_parts = os.path.basename(total_file).split(".")
+        country = filename_parts[0]
+        timestamp = filename_parts[1]
+        num_observations = 0
+
+        if (country in eurozone_countries) != eurozone:
+            continue
+
+        with open(total_file, "r") as infile:
+            reader = csv.reader(infile)
+            for row in reader:
+                if row[1] == "Total notes":
+                    continue
+                num_observations = int(row[1])
+                break
+           
+        if num_observations < minimum_observations:
+            continue
+
+        denom_file = "../../data/ebt-summary/{}.{}.denomination.csv".format(country, timestamp)
+        with open(denom_file, "r") as infile:
+            reader = csv.reader(infile)
+            data = dict()
+            data["Total"] = num_observations
+            for row in reader:
+                data[row[1]] = row[2]
+            country_data[country] = data
+
+    return country_data
+
+
+def absolute_to_relative(data):
+    outdata = dict()
+    for country, country_data in data.items():
+        out_country_data = dict()
+        for label, value in country_data.items():
+            if value == "Number":
+                continue
+            if label == "Total":
+                out_country_data[label] = int(value)
+            else:
+                out_country_data[label] = round(100* int(value) / int(country_data["Total"]), 1)
+        outdata[country] = out_country_data
+    return outdata
+
+
+def get_last_data(eurozone=True, minimum_observations = 300):
+    pass
+
+
+def get_color_dict():
+    return {   "5": "darkgreen",
+               "10": "red",
+               "20": "blue",
+               "50": "orange",
+               "100": "lightgreen",
+               "200": "yellow",
+               "500": "purple" }
+
+
+def plot_summary_data(data, outfile, eurozone=True):
+    countries = sorted(data.keys())
+    color_dict = get_color_dict()
+    plots = []
+    denominations = ["5", "10", "20", "50", "100", "200", "500"]
+    denomination_labels = list(map(lambda d: d + " euro", denominations))
+    cumulative_data = dict()
+    table_data = dict()
+    fig = plt.gcf()
+    plt.gca().invert_yaxis()
+    fig.set_size_inches(18.5, 10.5)
+    for denomination in denominations:
+        denom_data = []
+        table_data[denomination] = dict()
+        color = color_dict[denomination]
+        lefts = []
+        for country in countries:
+            table_data[country] = dict()
+            this_data = data[country].get(denomination, 0)
+            table_data[denomination][country] = this_data
+            if country in cumulative_data:
+                left = cumulative_data[country]
+                cumulative_data[country] += this_data
+            else:
+                left = 0
+                cumulative_data[country] = this_data
+            lefts.append(left)
+            denom_data.append(data[country].get(denomination, 0))
+        plot = plt.barh(countries, denom_data, color=color, left=lefts)
+        plots.append(plot)
+    plt.title("Tracked notes by denomination by country " +
+              "(inside eurozone)" if eurozone else "(outside eurozone)")
+    plt.legend(plots, denomination_labels, title="Denominations", loc="lower left")
+    plt.tight_layout()
+    plt.savefig(outfile)
+    plt.clf()
+    print("|Country|5 euro|10 euro|20 euro|50 euro|100 euro|200 euro|500 euro|")
+    print("|-------|------|-------|-------|-------|--------|--------|--------|")
+    for country in sorted(countries):
+        print("|{}|{}|{}|{}|{}|{}|{}|{}|".format(country,
+                table_data["5"][country],
+                table_data["10"][country],
+                table_data["20"][country],
+                table_data["50"][country],
+                table_data["100"][country],
+                table_data["200"][country],
+                table_data["500"][country]))
+    print()
+
+eurozone_data = absolute_to_relative(get_summary_data(eurozone=True))
+nonez_data = absolute_to_relative(get_summary_data(eurozone=False))
+plot_summary_data(eurozone_data, "../../plots/eurozone-summary.png", True)
+plot_summary_data(nonez_data, "../../plots/non-eurozone-summary.png", False)
